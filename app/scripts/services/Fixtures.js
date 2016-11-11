4a43b40491746ecefd671780f41dddeb83abecaa
imdoingotherthings <asaiandrade@yahoo.com>
2016-11-10 22:28:40 -0800

    Adding services to our app. Fixture.js script which contains our service calls for AlbumCtrl.js file and CollcetionCtrl.js file.

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -1,9 +1,13 @@
-var albumPicasso = {
+(function () {
+	function Fixtures() {
+		var Fixtures = {};
+		
+		var albumPicasso = {
     title: 'The Colors',
     artist: 'Pablo Picasso',
     label: 'Cubism',
 		year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
+    albumArtUrl: '/assets/images/album_covers/01.png',
 		songs: [
       { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
 			{ title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
@@ -13,31 +17,37 @@ var albumPicasso = {
     ]
 	};
 
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
+		var albumMarconi = {
+		title: 'The Telephone',
+		artist: 'Guglielmo Marconi',
+		label: 'EM',
+		year: '1909',
+		albumArtUrl: '/assets/images/album_covers/20.png',
 		songs: [
 			{ title: 'Hello, Operator?', duration: '1:01' },
 			{ title: 'Ring, ring, ring', duration: '5:01' },
 			{ title: 'Fits in your pocket', duration: '3:21'},
 			{ title: 'Can you hear me now?', duration: '3:14' },
 			{ title: 'Wrong phone number', duration: '2:15'}
-    ]
-  };
-
-var albumAfrojack = {
-    title: 'Its a matter of...', 
-    artist: 'Afrojack',
-    label: 'Wall Recordings',
-    year: '2013',
-    albumArtUrl: 'assets/images/album_covers/matterOf.jpg',
-    songs: [
-      { title: 'Ray Bomb', duration: '6:49' },
-			{ title: 'Rocker', duration: '5:28' },
-			{ title: 'Griever', duration: '6:11' },
-			{ title: 'Yubaba', duration: '4:54' }
-    ]
-  };
+			]
+		};	
+		
+		Fixtures.getAlbum = function () {
+			return albumPicasso;
+		};
+		
+		Fixtures.getCollection = function (numberOfAlbums) {
+			var albumArray = [];
+			for (var count = 0; count < numberOfAlbums; count++) {
+				albumArray.push(count[numberOfAlbums]);
+			}
+			return albumArray;
+		};
+		
+		return Fixtures;
+}
+	
+	angular
+		.module('blocJams'),
+		.factory('Fixtures', Fixtures);
+})();
