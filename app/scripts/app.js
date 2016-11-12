88e5120ee8c44bd789cc9386f2c541d0047a7444
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-11-12 07:07:24 -0800

    fixes to errors

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,30 +1,29 @@
 (function() {
-		function config($stateProvider, $locationProvider) {
-			$locationProvider
-				.html5Mode({
-					enable: true,
-					requireBase: false
+	function config($stateProvider, $locationProvider){
+		$locationProvider //config app's paths
+			.html5Mode({
+				enabled: true, //hides #! from url (hashbang indicates that the page load is triggered by JS)
+				requireBase: false
 			});
-			
-			$stateProvider
-				.state('landing', {
-					url: '/',
-					controller: 'LandingCtrl as landing',
-					templateUrl: '/templates/landing.html'
+		$stateProvider //determine the number of states and their properties
+			.state('landing', {
+				url: '/',
+				controller: 'LandingCtrl as landing',
+				templateUrl: '/templates/landing.html'
 			})
-			
-				.state('album', {
-					url: '/album',
-					templateUrl: '/templates/album.html'
-			})
-			
-				.state('collection', {
-					url: '/collection',
-					controller: 'CollectionCtrl as collection',
-					templateUrl: '/templates/collection.html'
-			});
-		}
+			.state('album', {
+             	url: '/album',
+             	controller: 'AlbumCtrl as album',
+             	templateUrl: '/templates/album.html'
+         	})
+         	.state('collection', {
+         		url: '/collection',
+         		controller: 'CollectionCtrl as collection',
+         		templateUrl: '/templates/collection.html'
+         	});
+	}
+
 	angular
 		.module('blocJams', ['ui.router'])
-		.config(config);
+		.config(config)
 })();
